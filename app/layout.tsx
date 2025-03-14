6ab3dbd04a9021a97cac322ca72a4189bfad9ca1
edusan999 <edosanchezcl@gmail.com>
2025-03-13 21:45:58 -0300

    Agregar layout principal

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'App-Visho-3D',
+  description: 'Aplicación de visualización 3D utilizando Three.js',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="es">
+      <body className={inter.className}>
+        {children}
+      </body>
+    </html>
+  )
+}
